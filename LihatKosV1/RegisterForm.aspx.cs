e672edc0c68131e83adf03480aea47971f26ac54
sonyTK <sony_ss4@yahoo.com>
2017-03-20 17:19:16 +0700

    start sendactivationemail

--- LihatKosV1/RegisterForm.aspx.cs
+++ LihatKosV1/RegisterForm.aspx.cs
@@ -15,7 +15,46 @@ namespace LihatKosV1
         {
 
         }
-
+        //https://www.aspsnippets.com/Articles/Send-user-Confirmation-email-after-Registration-with-Activation-Link-in-ASPNet.aspx
+        private void SendActivationEmail(int userId)
+        {
+            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            string activationCode = Guid.NewGuid().ToString();
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("INSERT INTO UserActivation VALUES(@UserId, @ActivationCode)"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.CommandType = CommandType.Text;
+                        cmd.Parameters.AddWithValue("@UserId", userId);
+                        cmd.Parameters.AddWithValue("@ActivationCode", activationCode);
+                        cmd.Connection = con;
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                        con.Close();
+                    }
+                }
+            }
+            using (MailMessage mm = new MailMessage("sender@gmail.com", txtEmail.Text))
+            {
+                mm.Subject = "Account Activation";
+                string body = "Hello " + txtUsername.Text.Trim() + ",";
+                body += "<br /><br />Please click the following link to activate your account";
+                body += "<br /><a href = '" + Request.Url.AbsoluteUri.Replace("CS.aspx", "CS_Activation.aspx?ActivationCode=" + activationCode) + "'>Click here to activate your account.</a>";
+                body += "<br /><br />Thanks";
+                mm.Body = body;
+                mm.IsBodyHtml = true;
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.gmail.com";
+                smtp.EnableSsl = true;
+                NetworkCredential NetworkCred = new NetworkCredential("sender@gmail.com", "<password>");
+                smtp.UseDefaultCredentials = true;
+                smtp.Credentials = NetworkCred;
+                smtp.Port = 587;
+                smtp.Send(mm);
+            }
+        }
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
             if (txtPassword.Text != txtPasswordConfirm.Text)
