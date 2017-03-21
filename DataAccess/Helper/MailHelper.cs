76da7b105c8205ba97c2d21eeb732132ae00ada8
sonyTK <sony_ss4@yahoo.com>
2017-03-21 17:05:30 +0700

    sendemail

--- DataAccess/Helper/MailHelper.cs
+++ DataAccess/Helper/MailHelper.cs
@@ -37,7 +37,18 @@ namespace LihatKos.DataAccess.Helper
                 throw;
             }
         }
-
+        public void SendEmail(string email, string subject, string message)
+        {
+            MailMessage mail = new MailMessage("noreply@psinformatika.com", email);
+            SmtpClient client = new SmtpClient();
+            //client.Port = 25;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            //client.UseDefaultCredentials = false;
+            //client.Host = "113.20.31.137";
+            mail.Subject = subject;
+            mail.Body = message;
+            client.Send(mail);
+        }
         public void SendEmailActivation(Int64 UserID, string Email, string UserName)
         {
             try
@@ -48,25 +59,32 @@ namespace LihatKos.DataAccess.Helper
                 db.AddInParameter(dbCommand, "ActivationCode", DbType.String, activationCode);
 
                 db.ExecuteNonQuery(dbCommand);
-                using (MailMessage mm = new MailMessage("AutoEmail@LihatKos.com", Email))
-                {
-                    mm.Subject = "Account Activation LihatKos.com";
-                    string body = "Hello " + UserName.Trim() + ",";
-                    string url = "http://demo.lihatkos.com/CS_Activation.aspx?ActivationCode=";
-                    body += "<br /><br />Please click the following link to activate your account";
-                    body += "<br /><a href = '" + url + activationCode + "'>Click here to activate your account.</a>";
-                    body += "<br /><br />Thanks";
-                    mm.Body = body;
-                    mm.IsBodyHtml = true;
-                    SmtpClient smtp = new SmtpClient();
-                    smtp.Host = "smtp.binus.ac.id";
-                    smtp.EnableSsl = true;
-                    NetworkCredential NetworkCred = new NetworkCredential("AutoEmail@LihatKos.com", "<password>");
-                    smtp.UseDefaultCredentials = true;
-                    smtp.Credentials = NetworkCred;
-                    smtp.Port = 587;
-                    smtp.Send(mm);
-                }
+
+                string body = "Hello " + UserName.Trim() + ",";
+                string url = "http://demo.lihatkos.com/CS_Activation.aspx?ActivationCode=";
+                body += "<br /><br />Please click the following link to activate your account";
+                body += "<br /><a href = '" + url + activationCode + "'>Click here to activate your account.</a>";
+                body += "<br /><br />Thanks";
+                SendEmail(Email, "Account Activation LihatKos.com", body);
+                //using (MailMessage mm = new MailMessage("AutoEmail@LihatKos.com", Email))
+                //{
+                //    mm.Subject = "Account Activation LihatKos.com";
+                //    string body = "Hello " + UserName.Trim() + ",";
+                //    string url = "http://demo.lihatkos.com/CS_Activation.aspx?ActivationCode=";
+                //    body += "<br /><br />Please click the following link to activate your account";
+                //    body += "<br /><a href = '" + url + activationCode + "'>Click here to activate your account.</a>";
+                //    body += "<br /><br />Thanks";
+                //    mm.Body = body;
+                //    mm.IsBodyHtml = true;
+                //    SmtpClient smtp = new SmtpClient();
+                //    smtp.Host = "smtp.binus.ac.id";
+                //    smtp.EnableSsl = true;
+                //    NetworkCredential NetworkCred = new NetworkCredential("AutoEmail@LihatKos.com", "<password>");
+                //    smtp.UseDefaultCredentials = true;
+                //    smtp.Credentials = NetworkCred;
+                //    smtp.Port = 587;
+                //    smtp.Send(mm);
+                //}
                 
             }
             catch (Exception ex)
